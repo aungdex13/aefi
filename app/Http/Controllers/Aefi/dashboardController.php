45c16ae61e05465e55575c3868da4245760183ba
aungdex13 <zest2206@gmail.com>
2021-01-15 14:07:09 +0700

    150164 1406 dashboard update

--- app/Http/Controllers/Aefi/dashboardController.php
+++ app/Http/Controllers/Aefi/dashboardController.php
@@ -17,12 +17,25 @@
 			$listProvince=$this->listProvince();
 			$count_month=$this->count_month();
 			$yearnow =  now()->year ;
-		 // dd($count_month);
+			$count_north = $this->count_north();
+			// dd($count_north)
+			$count_northeast = $this->count_northeast();
+			$count_central = $this->count_central();
+			$count_eastern = $this->count_eastern();
+			$count_south = $this->count_south();
+			$count_western = $this->count_western();
+		  // dd($count_south);
 			return view('AEFI.Apps.dashboard',compact(
 			 'count_prov',
 			 'listProvince',
 			 'count_month',
-			 'yearnow'
+			 'yearnow',
+			 'count_north',
+			 'count_northeast',
+			 'count_central',
+			 'count_eastern',
+			 'count_south',
+			 'count_western'
 		 ));
 		}
 		public function selectdatadash()
@@ -66,6 +79,190 @@
 										 ->get();
 			  // dd($count_month);
 			return $count_month;
+		}
+		protected function count_north(){
+			$yearnow =  now()->year;
+			$north = [	51,
+									50,
+									52,
+									53,
+									54,
+									55,
+									56,
+									57,
+									58,
+							];
+			 // dd($yearnow);
+			$count_north = DB::table('aefi_form_1')
+										 ->select(DB::raw('count(*) as count_north'))
+										 ->whereYear('date_entry', '=', "$yearnow")
+										 ->where('status','=',null)
+										 ->where('province','=',$north)
+										 // ->groupBy('month_entry')
+										 // ->groupBy('count_south')
+										 ->get();
+				// dd($count_north);
+			return $count_north;
+		}
+		protected function count_northeast(){
+			$yearnow =  now()->year;
+			$northeast = [42,
+									97,
+									30,
+									49,
+									48,
+									47,
+									33,
+									34,
+									35,
+									36,
+									37,
+									39,
+									40,
+									41,
+									31,
+									43,
+									44,
+									45,
+									46,
+									32,
+							];
+			 // dd($yearnow);
+			$count_northeast = DB::table('aefi_form_1')
+										 ->select(DB::raw('count(*) as count_northeast'))
+										 ->whereYear('date_entry', '=', "$yearnow")
+										 ->where('status','=',null)
+										 ->where('province','=',$northeast)
+										 // ->groupBy('month_entry')
+										 // ->groupBy('count_south')
+										 ->get();
+				// dd($count_month);
+			return $count_northeast;
+		}
+		protected function count_central(){
+			$yearnow =  now()->year;
+			 // dd($yearnow);
+			 $central = [	60,
+											74,
+											61,
+											73,
+											72,
+											67,
+											66,
+											65,
+											64,
+											62,
+											75,
+											14,
+											26,
+											18,
+											10,
+											11,
+											12,
+											19,
+											13,
+											17,
+											16,
+											15,
+										];
+			$count_central = DB::table('aefi_form_1')
+										 ->select(DB::raw('count(*) as count_central'))
+										 ->whereYear('date_entry', '=', "$yearnow")
+										 ->where('status','=',null)
+										 ->whereIn('province',$central)
+										 // ->groupBy('month_entry')
+										 // ->groupBy('count_south')
+										 ->get();
+				// dd($count_month);
+			return $count_central;
+		}
+		protected function count_eastern(){
+			$yearnow =  now()->year;
+			$eastern = [96,
+									95,
+									94,
+									93,
+									92,
+									91,
+									90,
+									86,
+									84,
+									83,
+									82,
+									81,
+									80,
+									85,
+							];
+			 // dd($yearnow);
+			$count_eastern = DB::table('aefi_form_1')
+										 ->select(DB::raw('count(*) as count_eastern'))
+										 ->whereYear('date_entry', '=', "$yearnow")
+										 ->where('status','=',null)
+										 ->where('province','=',$eastern)
+										 // ->groupBy('month_entry')
+										 // ->groupBy('count_south')
+										 ->get();
+				// dd($count_month);
+			return $count_eastern;
+		}
+		protected function count_western(){
+			$yearnow =  now()->year;
+			$western = [96,
+									95,
+									94,
+									93,
+									92,
+									91,
+									90,
+									86,
+									84,
+									83,
+									82,
+									81,
+									80,
+									85,
+							];
+			 // dd($yearnow);
+			$count_western = DB::table('aefi_form_1')
+										 ->select(DB::raw('count(*) as count_western'))
+										 ->whereYear('date_entry', '=', "$yearnow")
+										 ->where('status','=',null)
+										 ->where('province','=',$western)
+										 // ->groupBy('month_entry')
+										 // ->groupBy('count_south')
+										 ->get();
+				// dd($count_month);
+			return $count_western;
+		}
+		protected function count_south(){
+			$yearnow =  now()->year;
+			$south = [96,
+								95,
+								94,
+								93,
+								92,
+								91,
+								90,
+								86,
+								84,
+								83,
+								82,
+								81,
+								80,
+								85,
+							];
+			   // dd($south->province_code);
+			$count_south = DB::table('aefi_form_1')
+										 ->select(DB::raw('count(*) as count_south'))
+										 ->whereYear('date_entry', '=', "$yearnow")
+										 ->where('status','=',null)
+										 ->where('province','=',$south)
+										 // ->groupBy('month_entry')
+										 // ->groupBy('count_south')
+										 ->get();
+
+				 // dd($count_south);
+			return $count_south;
 		}
 		protected function listProvince(){
 			$province = DB::table('tbl_provinces')
